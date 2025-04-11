0ff136975523541d7fffd57a5794ccc3d81eacd2
vihanpereraux <vihanpereraux@gmail.com>
2025-04-11 02:25:27 +0100

    fix thumbnail preview

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -113,7 +113,7 @@ const cleanMovieDetails = (data: any[], arr: MoviesProps[],) => {
             id: item.id,
             original_language: item.original_language,
             popularity: item.popularity,
-            poster_path: 'https://image.tmdb.org/t/p/w300/' + item.poster_path,
+            poster_path: item.poster_path ? `https://image.tmdb.org/t/p/w300/${item.poster_path}`: `https://i.ibb.co/YTdfZHjX/no-preview.jpg`,
             overview: item.overview,
             release_date: item.release_date,
             vote_average: item.vote_average,
